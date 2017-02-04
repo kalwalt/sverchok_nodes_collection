586ff5a328de580a8b89d79f4c998f78b5857460
kalwalt <info@kalwaltart.it>
2017-02-04 22:31:22 +0100

    final version

--- scripts/icosphere-simple-script.py
+++ scripts/icosphere-simple-script.py
@@ -1,3 +1,10 @@
+'''
+simple script to generate an icosphere, useful for geometry deformation.
+You can set scale, translate and rotation transformation plugging a Matrix node 
+in thr 'mat' pin.
+script by @kalwalt https://github.com/kalwalt
+'''
+
 import bpy
 import bmesh
 from mathutils import Matrix
@@ -19,22 +26,25 @@ def sv_main(subdiv=2,diam=4.0,mat = []):
        
     # Make a new BMesh
     bm = bmesh.new()
+    #uncomment for debugging
+    #print("matrices plugged in: {0}".format(mat))
+    #print("length of matrix is: {0}".format(len(mat)))
     
-    print("matrices in: {0}".format(mat))
     matr = []
-    #we want the matrices so we put them in a Blender Matrix()
-    print("length of matrix is: {0}".format(len(mat)))
+    
     if mat:
-        
         
         for i,m in enumerate(mat):
             if mat[0]:
+                #we want the matrices so we put them in a Blender Matrix()
                 matr = Matrix(m)
-                print("matrices in for: {0}".format(m))
-                print("Blender Matrix is: {0}".format(matr))
+                #uncomment for debugging
+                #print("matrices in for: {0}".format(m))
+                #print("Blender Matrix is: {0}".format(matr))
     else:
-        matr = Matrix.Scale(1.0,1(1.0,1.0,1.0))
-        print("Cocooo! we have not a plugged matrix")
+        
+        matr = Matrix()
+        #print("Cocoooo! we have not a plugged matrix")
                    
     bmesh.ops.create_icosphere(bm, subdivisions=subdiv, diameter=diam, matrix=matr, calc_uvs=False)
                 
@@ -43,9 +53,6 @@ def sv_main(subdiv=2,diam=4.0,mat = []):
     verts_out.append(verts)
     edges_out.append(edges)
     faces_out.append(faces)
-    
-    #free the bmesh after computation
-    #bm.free()
     
     out_sockets = [
         ['v', 'verts', verts_out],
