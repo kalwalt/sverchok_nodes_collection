d9300703f30ea54a9c2e114d046d14d6d417a304
kalwalt <info@kalwaltart.it>
2017-04-20 12:42:03 +0200

    added pymcubes example

---
+++ scripts/pymcubes/pymcubes_test.py
@@ -0,0 +1,35 @@
+"""
+in bounds s d=10 n=2
+in iso_val s d=8 n=2
+in samples s d=100 n=2
+out vertices v
+out triangles s
+"""
+
+
+import numpy as np
+import sys
+mcubes_path = r"/usr/local/lib/python3.5/dist-packages" #it depend on your OS but just paste the path where is scipy
+if not mcubes_path in sys.path:
+    sys.path.append(mcubes_path)    
+import mcubes
+
+# Create the volume
+def f(x, y, z):
+    return x**2 + y**2 + z**2
+
+# Create a data volume (30 x 30 x 30)
+#X, Y, Z = np.mgrid[:100, :100, :100]
+# u = (X-50)**2 + (Y-50)**2 + (Z-50)**2 - 25**2
+
+# Extract the 0-isosurface
+#verts, tri = mcubes.marching_cubes(u, 0)
+
+# Extract the 16-isosurface
+verts, tri = mcubes.marching_cubes_func(
+        (-bounds, -bounds, -bounds), (bounds, bounds, bounds),  # Bounds
+        samples, samples, samples,              # Number of samples in each dimension
+        f,                          # Implicit function
+        iso_val)                         # Isosurface value
+
+vertices, triangles = verts.tolist(), tri.tolist()
