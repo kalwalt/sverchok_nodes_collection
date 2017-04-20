5b0cad984d2fbfa24925bc792ef23060e257ad50
kalwalt <info@kalwaltart.it>
2017-04-20 14:48:30 +0200

    metaballs_test example

---
+++ scripts/pymcubes/metaballs_test.py
@@ -0,0 +1,31 @@
+"""
+in bounds s d=10 n=2
+in iso_val s d=8.0 n=2
+in samples s d=100 n=2
+in factor s d=0.9 n=2
+out vertices v
+out triangles s
+"""
+
+
+import numpy as np
+import sys
+mcubes_path = r"/usr/local/lib/python3.5/dist-packages" #it depend on your OS but just paste the path where is scipy
+if not mcubes_path in sys.path:
+    sys.path.append(mcubes_path)    
+import mcubes
+import math
+
+# Create the volume
+def f(x, y, z):
+    
+    return ((x-factor)*(x-factor)+y*y+z*z-1)*((x+factor)*(x+factor)+y*y+z*z-1)-0.3
+
+# Extract the 16-isosurface
+verts, tri = mcubes.marching_cubes_func(
+        (-bounds, -bounds, -bounds), (bounds, bounds, bounds),  # Bounds
+        samples, samples, samples,              # Number of samples in each dimension
+        f,                          # Implicit function
+        iso_val)                         # Isosurface value
+
+vertices, triangles = verts.tolist(), tri.tolist()
