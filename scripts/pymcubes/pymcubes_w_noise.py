1e58d86630ae06fca6214311f82624813434f392
kalwalt <info@kalwaltart.it>
2017-04-25 14:59:05 +0200

    improved version of pymcubes_w_noise

--- scripts/pymcubes/pymcubes_w_noise.py
+++ scripts/pymcubes/pymcubes_w_noise.py
@@ -1,5 +1,6 @@
 """
 in bounds s d=10 n=2
+in size s d=1.0 n=2
 in iso_val s d=8.0 n=2
 in noise s d=[] n=0
 out vertices v
@@ -18,11 +19,11 @@ import mcubes
 
 
 # Create a data volume
-X, Y, Z = np.mgrid[:bounds, :bounds, :bounds]
+b = complex(bounds)
+X, Y, Z = np.mgrid[:size:b, :size:b, :size:b]
 n = np.array(noise)
 u = np.reshape(n, (bounds,bounds,bounds))
 v = Z * u
-
 # Extract the 0-isosurface
 verts, tri = mcubes.marching_cubes(v, iso_val)
 
